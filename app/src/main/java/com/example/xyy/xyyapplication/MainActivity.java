25c0db7240f1346993d2482d2b03a9bd4a6db85d
xiangdong.qu <qxd826@163.com>
2016-04-25 02:07:00 +0800

    xyy

--- app/src/main/java/com/example/xyy/xyyapplication/MainActivity.java
+++ app/src/main/java/com/example/xyy/xyyapplication/MainActivity.java
@@ -1,12 +1,22 @@
 package com.example.xyy.xyyapplication;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-public class MainActivity extends Activity{
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void initView() {
     }
 }
