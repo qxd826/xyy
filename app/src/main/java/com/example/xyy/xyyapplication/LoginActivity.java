5d97bfa08def896f4ee621f1762408c5f7461aab
xiangdong.qu <qxd826@163.com>
2016-04-25 00:04:17 +0800

    xyy

---
+++ app/src/main/java/com/example/xyy/xyyapplication/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.example.xyy.xyyapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by QXD on 2016/4/24.
+ */
+public class LoginActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+}
